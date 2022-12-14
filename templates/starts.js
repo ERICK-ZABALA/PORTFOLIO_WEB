2a23cd7c9dd3914b4fe8413c9367535c9c069564
name = ER!CK <38144008+ERICK-ZABALA@users.noreply.github.com>
2022-12-14 05:45:16 +0000

    video you tube

--- templates/starts.js
+++ templates/starts.js
@@ -1,31 +1,15 @@
-// Create a new canvas element and append it to the page
-var canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
-
-// Set the width and height of the canvas to match the window
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-// Get the drawing context from the canvas
-var ctx = canvas.getContext("2d");
-
-// Create a function that draws a star at a random position on the canvas
-function drawStar() {
-  // Generate a random number between 0 and 1
-  var x = Math.random();
-  var y = Math.random();
-
-  // Use the random numbers to calculate the position of the star
-  var starX = x * canvas.width;
-  var starY = y * canvas.height;
-
-  // Draw a small circle at the calculated position
-  ctx.beginPath();
-  ctx.arc(starX, starY, 2, 0, 2 * Math.PI);
-  ctx.fill();
-}
-
-// Draw 1000 stars on the canvas
-for (var i = 0; i < 1000; i++) {
-  drawStar();
-}
+function crearCampoEstrellas(numEstrellas) {
+    // Creamos un elemento div vacío para almacenar las estrellas
+    var divEstrellas = document.createElement('div');
+  
+    // Generamos las estrellas y las agregamos al elemento div
+    for (var i = 0; i < numEstrellas; i++) {
+      var estrella = document.createElement('i');
+      estrella.classList.add('fas', 'fa-star');
+      divEstrellas.appendChild(estrella);
+    }
+  
+    // Retornamos el elemento div con las estrellas
+    return divEstrellas;
+  }
+  
