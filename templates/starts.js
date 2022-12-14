8eb504e44370d8a942cc75abcaf5119ca527eed0
name = ER!CK <38144008+ERICK-ZABALA@users.noreply.github.com>
2022-12-14 05:07:01 +0000

    video you tube

--- templates/starts.js
+++ templates/starts.js
@@ -1,15 +1,7 @@
-// Obtener una referencia al elemento canvas y obtener un contexto de dibujo
-const canvas = document.getElementById('my-canvas');
-const ctx = canvas.getContext('2d');
-
-// Función para actualizar la simulación
-function update() {
-  // Mover los elementos en el canvas
-  // ...
-
-  // Redibujar el campo de fondo
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-// Actualizar la simulación cada 10 milisegundos
-setInterval(update, 10);
+function dibujarSimulacion() {
+    // Código para dibujar el campo y elementos dinámicos
+    }
+    
+    setInterval(dibujarSimulacion, 100); // Actualiza la simulación cada 100 milisegundos
+    
+    
