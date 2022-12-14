4a11b1170f6a7f0667fff7d4325e3682f8d110b5
name = ER!CK <38144008+ERICK-ZABALA@users.noreply.github.com>
2022-12-14 05:25:45 +0000

    video you tube

--- templates/starts.js
+++ templates/starts.js
@@ -1,7 +1,31 @@
-function dibujarSimulacion() {
-    // Código para dibujar el campo y elementos dinámicos
-    }
-    
-    setInterval(dibujarSimulacion, 100); // Actualiza la simulación cada 100 milisegundos
-    
-    
+// Create a new canvas element and append it to the page
+var canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+
+// Set the width and height of the canvas to match the window
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Get the drawing context from the canvas
+var ctx = canvas.getContext("2d");
+
+// Create a function that draws a star at a random position on the canvas
+function drawStar() {
+  // Generate a random number between 0 and 1
+  var x = Math.random();
+  var y = Math.random();
+
+  // Use the random numbers to calculate the position of the star
+  var starX = x * canvas.width;
+  var starY = y * canvas.height;
+
+  // Draw a small circle at the calculated position
+  ctx.beginPath();
+  ctx.arc(starX, starY, 2, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+// Draw 1000 stars on the canvas
+for (var i = 0; i < 1000; i++) {
+  drawStar();
+}
